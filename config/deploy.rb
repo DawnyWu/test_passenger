d8852f2b0833a9c48ea4098fc88051852078addd
Dawny Wu <superbatironmans5@gmail.com>
2014-07-16 18:13:58 +0800

    forward agent

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-
+# ssh_options[:forward_agent] = true
 # set :domain, '106.185.29.94'
 set :domain, 'www.zhaitx.com'
 set :deploy_to, '/var/www/zhaitx.com'
@@ -26,7 +26,7 @@ set :shared_paths, ['config/database.yml', 'log']
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
+set :forward_agent, true     # SSH forward_agent.
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
