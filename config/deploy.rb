6f2fc6555c9cf09370260919f500062eff44617b
Dawny Wu <superbatironmans5@gmail.com>
2014-07-16 18:18:24 +0800

    ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-# ssh_options[:forward_agent] = true
+ssh_options[:forward_agent] = true
 # set :domain, '106.185.29.94'
 set :domain, 'www.zhaitx.com'
 set :deploy_to, '/var/www/zhaitx.com'
