97364e2e61aaae80cee7621062750b01c718432b
Dawny Wu <superbatironmans5@gmail.com>
2014-07-16 18:24:27 +0800

    add ssh user

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-# ssh_options[:forward_agent] = true
+
 set :domain, '106.185.29.94'
 # set :domain, 'zhaitx.com'
 set :deploy_to, '/var/www/zhaitx.com'
@@ -24,7 +24,7 @@ set :branch, 'master'
 set :shared_paths, ['config/database.yml', 'log']
 
 # Optional settings:
-#   set :user, 'root'    # Username in the server to SSH to.
+  set :user, 'root'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
 # set :forward_agent, true     # SSH forward_agent.
 
