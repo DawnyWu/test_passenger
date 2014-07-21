de0f1df7c53cbfcfb2337ba2d0496d307d6906a4
Dawny Wu <superbatironmans5@gmail.com>
2014-07-21 10:26:07 +0800

    nothing

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,7 +67,7 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
 
     to :launch do
-      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      #queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
     end
   end
 end
